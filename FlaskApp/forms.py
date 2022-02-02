bcdeec9f0b1ba34bc2e736cf3ffe448b5cf825bb
Lartg <roc.gerrety@gmail.com>
2022-02-02 10:41:13 -0800

    installed some validation packages and made forms

---
+++ FlaskApp/forms.py
@@ -0,0 +1,55 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, SelectField, SubmitField, IntegerField
+from wtforms_sqlalchemy import fields
+from wtforms.validators import DataRequired, Length, ValidationError, NumberRange
+from FlaskApp.models import Post, User, Comment
+
+class PostForm(FlaskForm):
+  time_created = DateField
+  title = StringField('Post Title',
+    validators=[
+      DataRequired(),
+      Length(min=3, max = 50, messsage='Your Title needs to be within 3 and 50 characters')
+    ])
+  description = StringField('Post Description',
+    validators=[
+      DataRequired()
+    ])
+  owner = fields.QuerySelectField('Owner', query_factory=lambda: User.query, allow_blank=False)
+  submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+  username = StringField('Username',
+    DataRequired(),
+    Length(min=4, max=20, message=None))
+  password = StringField('Password',
+    DataRequired(),
+    Length(min=4, max=20, message=None))
+  submit = SubmitField('Submit')
+
+class NewUserForm(FlaskForm):
+  username = StringField('Username',
+    DataRequired(),
+    Length(min=4, max=20, message=None))
+  password = StringField('Password',
+    DataRequired(),
+    Length(min=4, max=20, message=None))
+  email = StringField('Email',
+    validators=[
+      DataRequired()
+    ])
+  name = StringField('Name',
+    validators=[
+      DataRequired()
+    ])
+  age = IntegerField('Age',
+    validators=[
+      DataRequired(),
+      NumberRange(min=18,max=None,message='Must be above 18 yeard old')
+      ])
+  profile_bio = StringField('Bio')
+  submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+  content = StringField('Comment')
+  submit = SubmitField('Submit')
--- FlaskApp/models.py
+++ FlaskApp/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String, nullable=False)
   password = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, nullable=False)
   name = db.Column(db.String, nullable=True)
   age = db.Column(db.String, nullable=True)
   profile_bio = db.Column(db.String, nullable=True)
