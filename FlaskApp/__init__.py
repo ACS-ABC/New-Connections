7fe0b42e6254474858e7224f78942e62339c7306
Lartg <roc.gerrety@gmail.com>
2022-02-09 11:43:17 -0800

    here you are christian

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_migrate import Migrate
 from sqlalchemy import create_engine
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -15,6 +17,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate.init_app(db, app)
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+from FlaskApp.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+
 
 from FlaskApp.routes import main
 
--- FlaskApp/routes.py
+++ FlaskApp/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, render_template, request, flash, redirect
 from FlaskApp.forms import CommentForm, LoginForm, PostForm, SignUpForm, EditAccountForm
 from FlaskApp.models import Post, User, Comment, Like
+from flask_login import current_user, login_required, login_user, logout_user
 from FlaskApp import db
 main = Blueprint('main', __name__)
 
@@ -23,13 +24,14 @@ def sign_up_page():
     db.session.commit()
     return redirect(f'/account-profile/{new_user.id}')
     
-  return render_template('login_page.html', form=form)
+  return render_template('sign_up_page.html', form=form)
 
 @main.route('/login', methods = ['GET', 'POST'])
 def login_page():
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(username=form.username)
+    login_user(user)
     if user:
       if user.password == form.password:
         user = user
@@ -42,7 +44,7 @@ def login_page():
 
 @main.route('/feed/<user_id>', methods = ['GET', 'POST'])
 def display_feed(user_id):
-  user = User.query.get(user_id)
+  user = current_user
   posts = Post.query.all()
   form = CommentForm()
   if form.validate_on_submit():
@@ -57,8 +59,8 @@ def display_feed(user_id):
   return render_template('feed.html', posts=posts, user=user)
 
 @main.route('/create-post/<user_id>', methods = ['GET', 'POST'])
+@login_required
 def create_post(user_id):
-  user = User.query.get(user_id)
   form = PostForm()
   if form.validate_on_submit():
     new_post = Post(
@@ -74,13 +76,15 @@ def create_post(user_id):
   return render_template('create_post.html')
 
 @main.route('/account-profile/<user_id>')
+@login_required
 def account_profile(user_id):
-  user = User.query.get(user_id)
+  user = current_user
   return render_template('account_profile.html', user=user)
 
 @main.route('/account-profile/<user_id>/edit', methods = ['GET', 'POST'])
+@login_required
 def edit_profile(user_id):
-  user = User.query.get(user_id)
+  user = current_user
   form = EditAccountForm(obj=user)
   if form.validate_on_submit():
     user.username = form.username.data
@@ -88,3 +92,9 @@ def edit_profile(user_id):
     user.profile_bio = form.profile_bio.data
     return redirect(f'/account-profile.html/{user_id}')
   return render_template('edit_profile.html', user=user, form=form)
+
+@main.route('/logout')
+@login_required
+def logout():
+  logout_user(current_user)
+  return redirect('/')
