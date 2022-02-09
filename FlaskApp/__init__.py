0a450c344e25657f2797fc9904b5b0021dfc680c
Lartg <roc.gerrety@gmail.com>
2022-02-09 10:56:09 -0800

    prayge

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from flask_migrate import Migrate
+from sqlalchemy import create_engine
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-db = SQLAlchemy(app)
+migrate = Migrate(compare_type=True)
+
+db = SQLAlchemy()
+uri = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate.init_app(db, app)
 
 from FlaskApp.routes import main
 
