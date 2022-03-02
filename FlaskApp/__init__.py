740547062d6b7a65828e9ba2cb0cded306d46305
Lartg <roc.gerrety@gmail.com>
2022-03-01 19:15:16 -1000

    changed port

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -15,15 +15,15 @@ migrate = Migrate(compare_type=True)
 
 db = SQLAlchemy()
 uri = os.environ.get('DATABASE_URI')
-print(uri)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-DATABASE_URL = uri
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(uri, sslmode='require')
 
 db.init_app(app)
+
 migrate.init_app(db, app)
 
 from FlaskApp.routes import main
